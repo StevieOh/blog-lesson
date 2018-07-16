cbb3a6795257cce513f0c91e99d5cc5ee1860c1d
Stephanie Obeytek <sobeytek@gmail.com>
2018-07-16 12:18:50 -0500

    author show page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,19 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const Author = require('../models/author');
 
 router.get('/', (req, res) => {
- res.render('authors/index.ejs') 
+  Author.find({}, (err, foundAuthors) => {
+    console.log(foundAuthors, "<----------foundAuthors")
+    res.render('authors/index.ejs', {
+      authors: foundAuthors
+    })
+ }) 
 });
 
+//=======================
+// NEW ROUTE
+//=======================
 
 router.get('/new', (req, res) => {
  res.render('authors/new.ejs') 
 });
 
+router.get('/:id', (req, res) => {
+ Author.findById(req.params.id, (err, foundAuthor) => {
+   res.render('authors/show.ejs',{
+    author: foundAuthor
+   })
+ })
+})
+    
+
 router.post('/', (req, res) => {
  console.log(req.body)
- res.send('server received request') 
+ Author.create(req.body, (err, createdAuthor) => {
+   console.log(createdAuthor, 'this is the createdAuthor');
+   res.redirect('/authors');
+ })
 });
+
+
 
 
 module.exports = router;
