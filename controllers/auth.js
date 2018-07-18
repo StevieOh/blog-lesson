b3112aca5e2300a08243149d01948115392a7aef
Stephanie Obeytek <sobeytek@gmail.com>
2018-07-18 16:34:30 -0500

    logout added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,13 +2,22 @@ const express = require('express');;;
 const router = express.Router();
 const User = require('../models/user')
 
+
+
+//==============================
+//    AUTHORIZATION ROUTE
+//==============================
 router.get('/', (req, res) => {
   res.render('auth/login.ejs', {
     message: req.session.message
   }); 
-
 })
 
+
+
+//==============================
+//    LOG IN
+//==============================
 router.post('/login', (req, res) => {
   //req.session is available on EVERY SINGLE REQUEST from the client
   console.log(req.session, '<----this is req.session') 
@@ -17,6 +26,21 @@ router.post('/login', (req, res) => {
   req.session.loggedIn = true;
   req.session.username = req.body.username;
   res.redirect('/articles');
+})
+
+
+
+//==============================
+//    LOG OUT
+//==============================
+router.get('/logout', (req, res) => {
+ req.session.destroy((err) => {
+   if(err){
+    res.send('err destroying session')
+   }else{
+    res.redirect('/auth')
+   }
+ }) 
 })
 
 
